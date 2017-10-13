700e4e5658a9529b2b9b65e589f9cc4abcd723f7
Jenna Solis <jennabsol@gmail.com>
2017-10-13 10:38:18 -0500

    add files first time

---
+++ js/home-page-controller.js
@@ -0,0 +1,19 @@
+const zac = {
+    "question":  
+    "image": 
+}
+
+const invisible = { 
+    "question": "Have you ever just wanted to disappear?"
+    "image": 
+}
+
+const wizard = {
+    "question": "Have you ever wanted to be powerful like Albus Dumbledore?"
+    "image": 
+}
+
+const animal = {
+    "question": "Have you ever wanted to be the king of the jungle?"
+    "image":  
+}
